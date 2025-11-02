d75f7d670c0aec8c00fe0e0aa54e5192a796e0d4
JacobMunterSGD <148461541+JacobMunterSGD@users.noreply.github.com>
2025-11-02 13:24:39 -0500

    Completed task 3 - verticality with blocks to remove gaps below cubes in the terrain
    Also added new branch for current milestone

--- Assets/Scripts/TerrainGeneration.cs
+++ Assets/Scripts/TerrainGeneration.cs
@@ -20,6 +20,8 @@ public class TerrainGeneration : MonoBehaviour
 
 	[SerializeField] List<CubeInfo> cubes = new();
 
+    [SerializeField] LayerMask cubeLayerMask;
+
     [Header("BiomeData")]
     public AllBiomeData biomeData;
 	[SerializeField] BiomeData biomeGenerationData;
@@ -30,7 +32,7 @@ public class TerrainGeneration : MonoBehaviour
 	{
         amountOfBiomes = biomeData.BiomeList.Count;
 
-		GenerateCubes();
+		//GenerateCubes();
 	}
 
 	void GenerateCubes()
@@ -42,6 +44,8 @@ public class TerrainGeneration : MonoBehaviour
 		cubes = CreateCubes();
 
         cubes = CreateVerticalCubes(cubes);
+
+        ColourCubes();
 
 		// NESTED FUNCTIONS FROM HERE
 
@@ -95,16 +99,12 @@ public class TerrainGeneration : MonoBehaviour
 
                     Vector3 position = new Vector3(_width, height, _length);
 
-
-                    GameObject _cube = Instantiate(cubePrefab, position, Quaternion.identity);
+					GameObject _cube = Instantiate(cubePrefab, position, Quaternion.identity);
 
                     CubeInfo newCubeInfo = new(_cube, position, _tempColor, _tempBiome);
 					_cube.AddComponent<CubeInfoGameobject>().cubeInfo = newCubeInfo;
 
 					tempCubes.Add(newCubeInfo);
-
-                    MeshRenderer mr = _cube.GetComponent<MeshRenderer>();
-                    mr.material.color = _tempColor;
 
 					//float _colorMultiplier = 1 / (float)heightDifference;
 					//mr.material.color = new Color(height * _colorMultiplier, height * _colorMultiplier, height * _colorMultiplier);
@@ -114,14 +114,73 @@ public class TerrainGeneration : MonoBehaviour
             return tempCubes;
         }
 
+        // go through each cube, check how far down the cubes next to it are, then create cubes below this one by how many that is.
+        // This will ensure that there are no gaps below/above cubes
+
 		List<CubeInfo> CreateVerticalCubes(List<CubeInfo> _cubes)
         {
             List<CubeInfo> _verticalCubes = new();
 
-            foreach(CubeInfo cube in _cubes)
+            List<int> cubesToAddList = new();
+
+            for (int i = 0; i < _cubes.Count; i++)
             {
+                CubeInfo cube = _cubes[i];
+
+                int cubesToAdd = 0;
+
+                for (int n = 0; n < 4; n++)
+                {
+                    float angleInRadians = n * 90 * Mathf.Deg2Rad;
+                    Vector3 directionVector = new Vector3(Mathf.Cos(angleInRadians), 0, Mathf.Sin(angleInRadians));
+
+                    Vector3 raycastStartPoint = cube.CubeGameObject.transform.position + directionVector;
+
+                    RaycastHit hit;
+                    Physics.Raycast(raycastStartPoint, Vector3.down, out hit, Mathf.Infinity, cubeLayerMask);
+
+                    int distanceToNextCube = (int)Mathf.Abs(raycastStartPoint.y - hit.point.y) + 1;
+
+                    if (distanceToNextCube > cubesToAdd && !(hit.point == Vector3.zero))
+                    {
+						print(i + " " + distanceToNextCube + " " + hit.point);
+
+						cubesToAdd = distanceToNextCube;
+                        //print(hit.point + " " + cubesToAdd);
+                        
+					}
+
+					//if (hit.point == Vector3.zero) cubesToAdd = 0;
+				}
+
+				cubesToAddList.Add(cubesToAdd);
 
             }
+
+			for (int i = 0; i < _cubes.Count; i++)
+            {
+
+                print(i + " " + cubesToAddList[i]);
+
+				CubeInfo cube = _cubes[i];
+
+				for (int n = 1; n < cubesToAddList[i] + 1; n++)
+				{
+					Vector3 positionOffset = new Vector3(0, -n, 0);
+
+					GameObject _cube = Instantiate(cubePrefab, cube.CubeGameObject.transform.position + positionOffset, Quaternion.identity);
+
+					CubeInfo newCubeInfo = new(_cube, _cube.transform.position, cube.Colour, cube.Biome);
+					_cube.AddComponent<CubeInfoGameobject>().cubeInfo = newCubeInfo;
+
+					_verticalCubes.Add(newCubeInfo);
+				}
+
+				foreach (CubeInfo _cube in _cubes)
+				{
+					_verticalCubes.Add(_cube);
+				}
+			}      
 
             return _verticalCubes;
         }
@@ -142,6 +201,15 @@ public class TerrainGeneration : MonoBehaviour
             print("something's not working if we're here");
             return Biomes.plains;
         }
+
+        void ColourCubes()
+        {
+            foreach(CubeInfo cube in cubes)
+            {
+				MeshRenderer mr = cube.CubeGameObject.GetComponent<MeshRenderer>();
+				mr.material.color = cube.Colour;
+			}			
+		}
 
     }
 
